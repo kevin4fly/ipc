e4a0348ceb646103a08050c871826f30770ecbb7
kevin <kevin4fly@gmail.com>
2014-01-02 21:40:22 +0800

    ipc: pipe implementation

--- pipe/pipe.c
+++ pipe/pipe.c
@@ -3,46 +3,43 @@
 #include  <unistd.h>
 #include  <errno.h>
 #include  <string.h>
-#include  <assert.h>
 #include  <sys/wait.h>
 
 
-/* run the command: "ls -al | nl" to test ipc pipe */
+/* run the command: "ls -al | grep pipe" to test ipc pipe */
 
 int main()
 {
     char * const command_ls[] = {"ls", "-al", 0};
-    char * const command_nl[] = {"nl", 0};
-    pid_t child_ls, child_nl;
+    char * const command_grep[] = {"grep","pipe", 0};
+    pid_t child_ls, child_grep;
     // pipe_fd[0] for read and pipe_fd[1] for write
     int pipe_fd[2];
     pipe(pipe_fd);
-    if( (child_nl = fork()) < 0 )
+    if( (child_grep = fork()) < 0 )
     {
-        printf("strerror: %s\n",strerror(errno));
         perror("perror");
-        assert(child_nl >= 0);
+        exit(EXIT_FAILURE);
     }
-    if( child_nl == 0 )
+    if( child_grep == 0 )
     {
-        //child_nl is for read
+        //child_grep is for read
         close(pipe_fd[1]);
         //redirect the stdin to pipe_fd[0]
         close(STDIN_FILENO);
         dup2(pipe_fd[0],STDIN_FILENO);
         close(pipe_fd[0]);
 
-        execvp("nl",command_nl);
-        printf("child_nl gets wrong!\n");
+        execvp("grep",command_grep);
+        printf("child_grep gets wrong!\n");
         exit(-1);
     }
     //child_ls is for write
     close(pipe_fd[0]);
     if( (child_ls = fork()) < 0 )
     {
-        printf("strerror: %s\n",strerror(errno));
         perror("perror");
-        assert(child_ls >= 0);
+        exit(EXIT_FAILURE);
     }
     if( child_ls == 0 )
     {
@@ -57,6 +54,6 @@ int main()
     }
 
     close(pipe_fd[1]);
-    waitpid(child_nl,NULL,0);
+    waitpid(child_grep,NULL,0);
     return 0;
 }
